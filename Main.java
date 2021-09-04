fa70a1520dfaa2171ef4ef281b1afe074c23bc46
Jose Luis Lirac <jl.lira@gmail.com>
2021-09-04 17:00:10 -0500

    Ejercicio 1

---
+++ Main.java
@@ -0,0 +1,7 @@
+import classes.Dog;
+public class Main {
+    public static void main(String args[]) {
+        Dog puppy = new Dog();
+        puppy.barking();
+    }    
+}
---
+++ classes/Dog.java
@@ -0,0 +1,10 @@
+package classes;
+public class Dog {
+    String breed;
+    int age;
+    String color;
+
+    public void barking() {
+        System.out.println("Woof!!!...");
+    }
+}
